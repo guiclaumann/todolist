938be34d037b479f920b46e7dc73dbe10465cab8
guiclaumann <guiclaumann@hotmail.com>
2024-09-09 15:29:47 -0300

    commit inicial

---
+++ src/main/java/com/claumann/todolist/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.claumann.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+@RequestMapping(path= "/meucontroller")
+public class HomeController{
+
+    @GetMapping
+    public String getMethodName() {
+        return "hello world";
+    }
+    @GetMapping(path= "/oi")
+    public String getMethodName1() {
+        return "hello world 2";
+}
+    @GetMapping(path= "/test")
+    public String getMethodName2(@RequestParam String id) {
+    return "hello world 3: "+ id;
+}
+}
