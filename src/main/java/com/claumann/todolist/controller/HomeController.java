9ff6ee550fb0dc488f24325e6ee178ef63422ae9
guiclaumann <guiclaumann@hotmail.com>
2024-09-09 15:41:44 -0300

    formantando codigo

--- src/main/java/com/claumann/todolist/controller/HomeController.java
+++ src/main/java/com/claumann/todolist/controller/HomeController.java
@@ -5,23 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
 @RestController
-@RequestMapping(path= "/meucontroller")
-public class HomeController{
+@RequestMapping(path = "/meucontroller")
+public class HomeController {
 
     @GetMapping
     public String getMethodName() {
         return "hello world";
     }
-    @GetMapping(path= "/oi")
+
+    @GetMapping(path = "/oi")
     public String getMethodName1() {
         return "testando path oi";
-}
-    @GetMapping(path= "/test")
+    }
+
+    @GetMapping(path = "/test")
     public String getMethodName2(@RequestParam String id) {
-    return "hello world 3: "+ id;
-}
+        return "hello world 3, tudo bem: " + id;
+    }
 }
